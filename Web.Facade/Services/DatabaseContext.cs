617d39a05903d9d2e66bec3768ec50d6bc5e98a9
fbashilov <fedbashilov@gmail.com>
2023-01-14 16:50:23 +0300

    Add db context class

---
+++ Web.Facade/Services/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Fedor Bashilov. All rights reserved.
+
+namespace Web.Facade.Services
+{
+    using Microsoft.EntityFrameworkCore;
+    using Web.Facade.Models;
+
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options) => this.Database.EnsureCreated();
+
+        public DbSet<MenuItem> Menu => this.Set<MenuItem>();
+    }
+}
