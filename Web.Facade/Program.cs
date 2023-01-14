21efa5d4825ca49078120c383d7536f8370c928b
fbashilov <fedbashilov@gmail.com>
2023-01-14 16:49:49 +0300

    Add solution & Web.Facade project

---
+++ Web.Facade/Program.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Fedor Bashilov. All rights reserved.
+
+namespace Web.Facade
+{
+    using Microsoft.AspNetCore;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHost(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+    }
+}
---
+++ Web.Facade/Startup.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Fedor Bashilov. All rights reserved.
+
+namespace Web.Facade
+{
+    using Microsoft.EntityFrameworkCore;
+    using Web.Facade.Services;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContextFactory<DatabaseContext>(options =>
+            {
+                options.UseSqlServer(this.Configuration.GetConnectionString("DatabaseConnection"));
+                options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+            });
+            services.AddAdminServices();
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
