104391128a427bde33dd3ecd6f4eb9d4e6a827bc
fbashilov <fedbashilov@gmail.com>
2023-01-15 12:03:25 +0300

    Add logging

--- Web.Facade/Controllers/MenuController.cs
+++ Web.Facade/Controllers/MenuController.cs
@@ -24,8 +24,11 @@ namespace Web.Facade.Controllers
         [Route("")]
         public async Task<IActionResult> GetAllMenu()
         {
+            this.logger.LogInformation($"Starting to get all menu...");
             var menu = await this.menuService.GetAllMenu();
-            return this.Ok(JsonSerializer.Serialize(menu));
+
+            this.logger.LogInformation($"All menu received successfully! Menu: {JsonSerializer.Serialize(menu)}. Sending the menu in response...");
+            return this.Ok(menu);
         }
 
         [HttpGet]
@@ -34,11 +37,15 @@ namespace Web.Facade.Controllers
         {
             try
             {
+                this.logger.LogInformation($"Starting to get menu item with id = {id} ...");
                 var menuItem = await this.menuService.GetMenuItem(id);
-                return this.Ok(JsonSerializer.Serialize(menuItem));
+
+                this.logger.LogInformation($"The menu item with id = {id} received successfully! Menu item: {JsonSerializer.Serialize(menuItem)}. Sending the menu item in response...");
+                return this.Ok(menuItem);
             }
-            catch (NotFoundException)
+            catch (NotFoundException ex)
             {
+                this.logger.LogWarning(ex, $"Can't get menu item. Not found menu item with id = {id}. Sending 404 response...");
                 return this.NotFound($"Can't get menu item. Not found menu item with id = {id}");
             }
         }
@@ -47,7 +54,10 @@ namespace Web.Facade.Controllers
         [Route("")]
         public async Task<IActionResult> CreateMenuItem([FromBody] MenuItem newItem)
         {
+            this.logger.LogInformation($"Starting to create menu item: {JsonSerializer.Serialize(newItem)} ...");
             var menuItem = await this.menuService.CreateMenuItem(newItem);
+
+            this.logger.LogInformation($"The menu item created successfully! Menu item: {JsonSerializer.Serialize(menuItem)}. Sending the menu item in response...");
             return this.StatusCode(201, menuItem);
         }
 
@@ -57,20 +67,27 @@ namespace Web.Facade.Controllers
         {
             try
             {
+                this.logger.LogInformation($"Starting to update menu item with id = {id}. Menu item = {JsonSerializer.Serialize(newItem)} ...");
                 var menuItem = await this.menuService.UpdateMenuItem(id, newItem);
+
+                this.logger.LogInformation($"The menu item with id = {id} updated successfully! Menu item: {JsonSerializer.Serialize(menuItem)}. Sending the menu item in response...");
                 return this.Ok(menuItem);
             }
-            catch (NotFoundException)
+            catch (NotFoundException ex)
             {
+                this.logger.LogWarning(ex, $"Can't update menu item. Not found menu item with id = {id}. Sending 404 response...");
                 return this.NotFound($"Can't update menu item. Not found menu item with id = {id}");
             }
         }
 
         [HttpDelete]
         [Route("{id}")]
-        public async Task<IActionResult> DeletetMenuItem([FromRoute] int id)
+        public async Task<IActionResult> DeleteMenuItem([FromRoute] int id)
         {
+            this.logger.LogInformation($"Starting to delete menu item with id = {id} ...");
             await this.menuService.DeleteMenuItem(id);
+
+            this.logger.LogInformation($"The menu item with id = {id} deleted successfully! Sending 204 response...");
             return this.NoContent();
         }
     }
