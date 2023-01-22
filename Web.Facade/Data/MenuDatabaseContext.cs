5dd3e095b3e7ba24ee2c62989bef7f2e3a7bb052
fbashilov <fedbashilov@gmail.com>
2023-01-22 04:52:08 +0300

    Rename db context

---
+++ Web.Facade/Data/MenuDatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Fedor Bashilov. All rights reserved.
+
+namespace Web.Facade.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Web.Facade.Models;
+
+    public class MenuDatabaseContext : DbContext
+    {
+        public MenuDatabaseContext(DbContextOptions<MenuDatabaseContext> options)
+            : base(options) => this.Database.EnsureCreated();
+
+        public DbSet<MenuItem> Menu => this.Set<MenuItem>();
+    }
+}
