cd7516fc084e915b5d171a12095d3b50cd57bf3f
fbashilov <fedbashilov@gmail.com>
2023-01-22 04:51:37 +0300

    Add JWT settings

---
+++ Web.Facade/JWTSettings.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Fedor Bashilov. All rights reserved.
+
+namespace Web.Facade
+{
+    public class JWTSettings
+    {
+        public string? SecretKey { get; set; }
+
+        public string? Issuer { get; set; }
+
+        public string? Audience { get; set; }
+    }
+}
